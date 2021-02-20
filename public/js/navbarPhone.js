32a68ea557881c8a6bda05b2c643e1aa230c91cd
rzmsv <rmussavi@gmail.com>
2021-02-20 15:25:08 +0330

    create responsive

---
+++ public/js/navbarPhone.js
@@ -0,0 +1,11 @@
+
+var x = document.getElementById("myLinks");
+x.addEventListener("click",()=>{
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+})
+
+  
